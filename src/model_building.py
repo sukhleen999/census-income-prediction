7f059d5a4a64527a8b822ff240844345176466a1
philschan <philsonchan@gmail.com>
2021-11-25 15:55:34 -0800

    Migrate model building code to model_building.py

--- src/model_building.py
+++ src/model_building.py
@@ -10,9 +10,21 @@ Options:
 '''
 
 from docopt import docopt
+import numpy as np
 import os
 import pandas as pd
 import pickle
+from sklearn.compose import make_column_transformer
+from sklearn.dummy import DummyClassifier
+from sklearn.ensemble import RandomForestClassifier
+
+from sklearn.model_selection import (
+    cross_validate, RandomizedSearchCV
+)
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import (
+    StandardScaler, OneHotEncoder
+)
 import sys
 
 opt = docopt(__doc__)
@@ -57,9 +69,71 @@ def main():
     train_df = pd.read_csv(opt["<input_train_file>"])
     train_df = upfront_transform(train_df)
 
-    print(train_df, output_filename)
+    # Split data into features & target
+    X_train = train_df.drop("income", axis=1)
+    y_train = train_df['income']
 
+    # Preprocessing
+    numeric_feats = ['age', 'fnlwgt', 'hours_per_week']
+    categorical_null_feats = ['workclass', "occupation"]
+    categorical_nonull_feats = ["marital_status", "relationship"]
 
+    binary_feats = ['sex', 'native_country']
+    passthrough_feats = ['education_num']
+    drop_feats = ['education', 'race', 'capital_gain', 'capital_loss']
+
+    col_trans = make_column_transformer(
+        (StandardScaler(), numeric_feats),
+        (OneHotEncoder(sparse=False, handle_unknown='ignore', drop=[np.nan] * 2), categorical_null_feats),
+        (OneHotEncoder(sparse=False, handle_unknown='ignore'), categorical_nonull_feats),
+        (OneHotEncoder(drop='if_binary'), binary_feats),
+        ('passthrough', passthrough_feats),
+        ('drop', drop_feats)
+    )
+
+    # Calculate Baseline Performances
+    scoring = ['accuracy', 'precision', 'recall', 'f1']
+    baseline_results = {}
+    pipe_dummy = make_pipeline(
+        col_trans,
+        DummyClassifier()
+    )
+    baseline_results['DummyClassifier'] = pd.DataFrame(cross_validate(pipe_dummy, X_train, y_train, scoring=scoring)).mean()
+
+    # Calculate Baseline Perfromance for Random Forest Classifier
+    pipe_forest = make_pipeline(
+        col_trans,
+        RandomForestClassifier(random_state=522)
+    )
+    baseline_results['RandomForest_default'] = pd.DataFrame(cross_validate(pipe_forest, X_train, y_train, scoring=scoring)).mean()
+
+    # TODO: Export baseline_results
+
+    # Hyperparameter Tuning
+    param_dist = {
+        "randomforestclassifier__class_weight": [None, "balanced"],
+        "randomforestclassifier__n_estimators": [10, 20, 50, 100, 200, 500],
+        "randomforestclassifier__max_depth": np.arange(10, 20, 2)
+    }
+    rand_search_rf = RandomizedSearchCV(pipe_forest, param_dist, n_iter=20, 
+                                        random_state=952, scoring=scoring, refit="f1")
+
+    rand_search_rf.fit(X_train, y_train)
+
+    hyperparam_result = pd.DataFrame(
+        rand_search_rf.cv_results_
+    ).sort_values("rank_test_f1")[['param_randomforestclassifier__n_estimators',
+                                        'param_randomforestclassifier__max_depth',
+                                        'param_randomforestclassifier__class_weight',
+                                        'mean_test_accuracy',
+                                        'mean_test_precision',
+                                        'mean_test_recall',
+                                        'mean_test_f1',
+                                        ]]
+    # TODO: Export hyperparam_result
+
+    # TODO: Export the model
+    save_model(rand_search_rf, output_filename)
 
 
 if __name__ == "__main__":
